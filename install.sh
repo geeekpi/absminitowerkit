c43f8ab8b4ba95c972c3c07a47679a7598e8b568
yoyojacky <yoyojacky2009@gmail.com>
2022-07-05 16:37:51 +0800

    update install.sh
    Signed-off-by: yoyojacky <yoyojacky2009@gmail.com>

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 
 . /lib/lsb/init-functions
-sudo apt update && sudo apt-get -q install git  || echo "please check internet connection and make sure it can access internet!" 
+sudo apt update && sudo apt -y -q install git cmake scons  || log_action_msg "please check internet connection and make sure it can access internet!" 
 
 # install libraries. 
 log_action_msg "Check dependencies and install deps packages..."
@@ -11,19 +11,18 @@ sudo apt -y install python3 python3-pip python3-pil libjpeg-dev zlib1g-dev libfr
 sudo usermod -a -G gpio,i2c pi && log_action_msg "grant privilledges to user pi" || log_warning_msg "Grant privilledges failed!" 
 
 # download driver from internet 
-cd /usr/local/ && git clone https://github.com/geeekpi/absminitowerkit && cd absminitowerkit/luma.examples/  && sudo -H pip3 install -e . && log_action_msg "Install dependencies packages successfully..." || log_warning_msg "Cound not access github repository, please check the internet connections!!!" 
+cd /usr/local/ 
+if [ ! -d luma.examples ]; then
+   git clone https://github.com/rm-hull/luma.examples.git && cd luma.examples/ || log_warning_msg "Could not download repository from github, please check the internet connection..." 
+   cd luma.examples/  && sudo -H pip3 install -e . && log_action_msg "Install dependencies packages successfully..." || log_warning_msg "Cound not access github repository, please check the internet connections!!!" 
+fi 
 
 # download rpi_ws281x libraries.
-cd /usr/local/ && git clone https://github.com/geeekpi/absminitowerkit && log_action_msg "Download moodlight driver finished..." || log_warning_msg "Could not access github repository, please check the internet connections!!!" 
-if [ $? -eq 0 ]; then
-   log_action_msg "Install mood light driver..."
-   sudo apt -y -q install scons && cd absminitowerkit/rpi_ws281x/ && sudo scons && mkdir build && cd build/ && cmake -D BUILD_SHARED=OFF -D BUILD_TEST=ON .. && sudo make install && sudo cp ./test /usr/bin/moodlight  && log_action_msg "Installation finished..." || log_warning_msg "Installation process failed! Please try again..."
+cd /usr/local/ 
+if [ ! -d rpi_ws281x ]; then
+   git clone https://github.com/jgarff/rpi_ws281x && log_action_msg "Download moodlight driver finished..." || log_warning_msg "Could not access github repository, please check the internet connections!!!" 
+   cd rpi_ws281x/ && sudo scons && mkdir build && cd build/ && cmake -D BUILD_SHARED=OFF -D BUILD_TEST=ON .. && sudo make install && sudo cp ./test /usr/bin/moodlight  && log_action_msg "Installation finished..." || log_warning_msg "Installation process failed! Please try again..."
 fi
-
-# create minitower systemd service.
-daemon=minitower
-minitower_svc_file=/lib/systemd/system/$daemon.service
-
 
 # Enable i2c function on raspberry pi.
 log_action_msg "Enable i2c on Raspberry Pi "
@@ -35,47 +34,80 @@ fi
 
 # install minitower service.
 log_action_msg "Minitower service installation begin..."
-if [ -d /usr/local/luma.examples/ ]; then
-   log_action_msg "OLED driver install successfully"
-fi
 
 if [ -f /usr/bin/moodlight ]; then
    log_action_msg "moodlight driver install successfully"
 fi
 
-# send signal to MCU before system shuting down.
-echo "[Unit]" > ${minitower_svc_file}
-echo "Description=Minitower Service" >> $minitower_svc_file
-echo "DefaultDependencies=no" >> $minitower_svc_file
-echo "StartLimitIntervalSec=60" >> $minitower_svc_file
-echo "StartLimitBurst=5" >> $minitower_svc_file
+# mood light service.
+moodlight_svc="minitower_moodlight"
+moodlight_svc_file="/lib/systemd/system/${moodlight_svc}.service"
+
+echo "[Unit]" > ${moodlight_svc_file}
+echo "Description=Minitower mood light Service " >> ${moodlight_svc_file}
+echo "DefaultDependencies=no " >> ${moodlight_svc_file}
+echo "StartLimitIntervalSec=60 " >> ${moodlight_svc_file}
+echo "StartLimitBurst=5 " >> ${moodlight_svc_file}
+echo "[Service] " >> ${moodlight_svc_file}
+echo "RootDirectory=/ " >> ${moodlight_svc_file}
+echo "User=root " >> ${moodlight_svc_file}
+echo "Type=simple " >> ${moodlight_svc_file}
+echo "ExecStart=sudo /usr/bin/moodlight &  " >> ${moodlight_svc_file}
+echo "RemainAfterExit=yes " >> ${moodlight_svc_file}
+echo "Restart=always " >> ${moodlight_svc_file}
+echo "RestartSec=30 " >> ${moodlight_svc_file}
+echo "[Install] " >> ${moodlight_svc_file}
+echo "WantedBy=multi-user.target" >> ${moodlight_svc_file}
+
+log_action_msg "Minitower moodlight service installation finished." 
+sudo chown root:root ${moodlight_svc_file}
+sudo chmod 644 ${moodlight_svc_file}
+
+log_action_msg "Minitower moodlight Service Load module." 
+sudo systemctl daemon-reload
+sudo systemctl enable ${moodlight_svc}.service
+sudo systemctl restart ${moodlight_svc}.service
 
 
-echo "[Service]" >> $minitower_svc_file
-echo "RootDirectory=/" >> $minitower_svc_file
-echo "User=root" >> $minitower_svc_file
-echo "Type=simple" >> $minitower_svc_file
-echo "ExecStart=sudo /usr/bin/python3 /usr/local/luma.example/examples/sys_info.py & " >> $minitower_svc_file
-echo "ExecStart=sudo /usr/bin/moodlight &" >> $minitower_svc_file
-echo "RemainAfterExit=yes" >> $minitower_svc_file
-echo "Restart=on-failure" >> $minitower_svc_file
-echo "RestartSec=30" >> $minitower_svc_file
+# oled screen display service.
+oled_svc="minitower_oled"
+oled_svc_file="/lib/systemd/system/${oled_svc}.service"
 
-echo "[Install]" >> $minitower_svc_file
-echo "WantedBy=multi-user.target" >> $minitower_svc_file
+if [ -d /usr/local/luma.examples ]; then
+  echo "[Unit]" > ${oled_svc_file}
+  echo "Description=Minitower Service" >> ${oled_svc_file}
+  echo "DefaultDependencies=no" >> ${oled_svc_file}
+  echo "StartLimitIntervalSec=60" >> ${oled_svc_file}
+  echo "StartLimitBurst=5" >> ${oled_svc_file}
+  
+  
+  echo "[Service]" >> ${oled_svc_file}
+  echo "RootDirectory=/tmp" >> ${oled_svc_file}
+  echo "User=root" >> ${oled_svc_file}
+  echo "Type=simple" >> ${oled_svc_file}
+  echo "ExecStart=sudo /usr/bin/python3 /usr/local/luma.examples/examples/sys_info.py & " >> ${oled_svc_file}
+  echo "RemainAfterExit=yes" >> ${oled_svc_file}
+  echo "Restart=always" >> ${oled_svc_file}
+  echo "RestartSec=30" >> ${oled_svc_file}
+  
+  echo "[Install]" >> ${oled_svc_file}
+  echo "WantedBy=multi-user.target" >> ${oled_svc_file}
 
 log_action_msg "Minitower Service configuration finished." 
-sudo chown root:root $minitower_svc_file
-sudo chmod 644 $minitower_svc_file
+sudo chown root:root ${oled_svc_file}
+sudo chmod 644 $oled_svc_file
 
 log_action_msg "Minitower Service Load module." 
 sudo systemctl daemon-reload
-sudo systemctl enable $daemon.service
-sudo systemctl restart $daemon.service
+sudo systemctl enable ${oled_svc}.service
+sudo systemctl restart ${oled_svc}.service
 
 # Finished 
 log_success_msg "Minitower service installation finished successfully." 
 # greetings and require rebooting system to take effect.
 log_action_msg "Have fun!" 
 sudo sync
+else 
+  log_warning_msg "Installation failed due to can not download the repository from github..."
+fi
 
